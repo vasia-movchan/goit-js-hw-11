0ce6a844dbe078594364cae5394524fe5cdad1ae
Vasiliy Movchan <movchan.vasia@gmail.com>
2022-06-15 09:27:19 +0300

    hw-11 done

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const KEY_API = '28048019-183967a3cd32f0dfa8a2bcd0a';
 const BASE_URL = 'https://pixabay.com/api/';
 let page = 1;
+let totalHits = 0;
 
 const gallery = document.querySelector('.gallery');
 const form = document.querySelector('.search-form');
+const buttonLoadMore = document.querySelector('.load-more');
 
 form.addEventListener('submit', searchImages);
+buttonLoadMore.addEventListener('click', loadMore);
 
 const optionsQuery = {
   baseURL: `${BASE_URL}`,
@@ -20,13 +23,14 @@ const optionsQuery = {
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-    page: `${page}`,
-    per_page: 40,
+    page: 1,
+    per_page: 200,
   },
 };
 
 function searchImages(event) {
   event.preventDefault();
+  optionsQuery.params.page = 1;
   optionsQuery.params.q = event.target.elements.searchQuery.value;
 
   gallery.innerHTML = '';
@@ -35,7 +39,8 @@ function searchImages(event) {
 }
 
 async function getDataFromAPI() {
-  const res = await axios(optionsQuery).then(res => res.data.hits);
+  const res = await axios(optionsQuery).then(res => res.data);
+
   return res;
 }
 
@@ -75,7 +80,7 @@ function createGaleryItemMarkup(
 
 function createGalleryMarkup() {
   getDataFromAPI().then(res => {
-    const galleryMarkup = res
+    const galleryMarkup = res.hits
       .map(
         ({
           webformatURL,
@@ -98,7 +103,52 @@ function createGalleryMarkup() {
       )
       .join('');
 
-    gallery.insertAdjacentHTML('beforeend', galleryMarkup);
-    let lightbox = new SimpleLightbox('.gallery a');
+    if (galleryMarkup.length !== 0) {
+      gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+      buttonLoadMore.classList.remove('is-hidden');
+
+      infoOfSearchResult(res.totalHits);
+      infoOfEnd(res.totalHits);
+
+      let lightbox = new SimpleLightbox('.gallery a');
+    } else {
+      Notiflix.Notify.failure(
+        `Sorry, there are no images matching your search query. Please try again.`,
+        {
+          timeout: 6000,
+        }
+      );
+    }
   });
+}
+
+function loadMore() {
+  optionsQuery.params.page += 1;
+
+  createGalleryMarkup();
+}
+
+function infoOfEnd(totalHits) {
+  page = optionsQuery.params.page;
+  const countImagesPerPage = optionsQuery.params.per_page;
+  const countImages = page * countImagesPerPage;
+
+  if (countImages > totalHits) {
+    buttonLoadMore.classList.add('is-hidden');
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results.",
+      {
+        timeout: 6000,
+      }
+    );
+  }
+}
+
+function infoOfSearchResult(totalHits) {
+  if (optionsQuery.params.page === 1) {
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`, {
+      timeout: 6000,
+    });
+  }
 }
